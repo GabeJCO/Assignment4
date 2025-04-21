b6883ee6430e990787ede1b781add955c4b0b090
GabeJCO <gcorr032@fiu.edu>
2025-04-21 00:19:16 -0400

    Update optimal.c

--- optimal.c
+++ optimal.c
@@ -2,122 +2,218 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+
 #define ALLOCATED_FRAMES 7
-#define lrand() ((long)rand()*RAND_MAX + (long)rand())
 #define STRING_LEN 1000000
-//simulation parameters:
-int e;//Small integer representing current locus to simulate spatial locality!
-int m;//rate of motion - constant rate of moving locus forward!
-int P;//The virtual memory of a process consists of P pages, numbered 0 through P −1.
-double t;//probability of transitioning to a new locus
-int locate(int* frames, int size, int page_no){
-	
-	for(int i = 0; i < size;i++)
-		if(frames[i] == page_no)
-			return i;
-	return -1;
+#define lrand() ((long)rand()*RAND_MAX + (long)rand())
+
+// Function to locate a page in the frames
+int locate(int* frames, int size, int page_no) {
+    for (int i = 0; i < size; i++)
+        if (frames[i] == page_no)
+            return i;
+    return -1;
 }
-void ref_string_generator(int* ref_str){
-	//initially locus: 0...e-1
-  int locus_position = 0;//s
-  int ref_count = 0;
-  while(ref_count <= STRING_LEN){
-    int next_ref = lrand()%e + locus_position;
-    if(!ref_count || next_ref != ref_str[ref_count-1])//different from previous one
-      ref_str[ref_count++] = next_ref;
-    else//try again
-      continue;
-    if(ref_count%m == 0){//let's displace the locus
-      if(rand() < t * RAND_MAX)//jump
-        locus_position = lrand()%(P-e+1);
-      else
-        locus_position = (locus_position + 1)%(P-e+1);
+
+// Function to generate the reference string
+void ref_string_generator(int* ref_str, int P, int e, int m, double t) {
+    int locus_position = 0; // starting position
+    int ref_count = 0;
+
+    while (ref_count < STRING_LEN) {
+        int next_ref = lrand() % e + locus_position;
+        if (!ref_count || next_ref != ref_str[ref_count - 1]) // different from previous one
+            ref_str[ref_count++] = next_ref;
+        else
+            continue;
+
+        if (ref_count % m == 0) { // displace the locus
+            if (rand() < t * RAND_MAX) // jump
+                locus_position = lrand() % (P - e + 1);
+            else
+                locus_position = (locus_position + 1) % (P - e + 1);
+        }
     }
-  }
-  
 }
-int handle_clas(int argc, char* argv[]){
-	while(*++argv){
-    //./main -P 1048576 -e 10 -m 200 -t 2000 (Scenario #1)
-		if(**argv != '-')
-		  return 1;
-		switch((*argv)[1]){
-		  case 'P':
-		  P = atoi(*++argv);
-		  break;
-		  case 'e':
-		  e = atoi(*++argv);
-		  break;
-		  case 'm':
-		  m = atoi(*++argv);
-		  break;
-		  case 't'://inverse of transition probability
-		  t = 1.0/atoi(*++argv);
-		  break;
-		  default: //error
-		  return 1;
-		}
-	}
+
+// Optimal page replacement algorithm
+int optimal(int* ref_str, int size, int limit) {
+    int page_faults = 0;
+    int frames[size], i, cur, page_no, frame_no;
+
+    for (i = 0; i < size; i++)
+        frames[i] = -1; // empty frames
+
+    for (i = 0, cur = 0; i < size; i++, cur++) {
+        page_no = ref_str[cur];
+        frame_no = locate(frames, size, page_no);
+        if (frame_no >= 0) // already exists
+            i--;
+        else { // page fault
+            frames[i] = page_no;
+            page_faults++;
+        }
+    }
+
+    for (; cur < STRING_LEN; cur++) {
+        page_no = ref_str[cur];
+        frame_no = locate(frames, size, page_no);
+        if (frame_no >= 0) // already exists
+            continue;
+
+        page_faults++;
+        unsigned unused = (1 << size) - 1; // binary 1111111
+        int victim;
+
+        for (int k = 1; k <= limit && unused && cur + k < STRING_LEN; k++) {
+            victim = locate(frames, size, ref_str[cur + k]);
+            if (victim >= 0)
+                unused &= ~(1 << victim); // set unused bit to zero
+        }
+
+        if (!unused) { // all frames will be referred to in the future
+            frames[victim] = page_no;
+        } else { // at least one frame won't be referred to in the future
+            victim = 0;
+            while (unused % 2 == 0) {
+                unused = unused >> 1;
+                victim++;
+            }
+            frames[victim] = page_no;
+        }
+    }
+    return page_faults;
 }
-int optimal(int* ref_str, int size, int limit){
-  //size is the # of frames allocated for the process
-  //limit is the max # of cells that we look ahead in the RS to implement the optimal algorithm
-  //initialize
-	int page_faults = 0;
-	int frames[size], i, cur, page_no, frame_no;
-	for(i = 0; i < size;i++)
-		frames[i] = -1;//empty
-	//PURE demand paging means all frames are empty!
-	//Let's fill them out with the first few numbers in the reference string!
-	for(i = 0, cur = 0; i < size;i++,cur++){
-		page_no = ref_str[cur];//get the next vpn from reference string
-		frame_no = locate(frames, size, page_no);//check if it is in the memory!
-		if(frame_no >= 0)//already exists in main memory, duplicate!
-			i--;
-		else{//page-fault, swap it in to the next available position!
-			frames[i] = page_no;
-			page_faults++;
-		}
-	}
-	//main loop... now ALL frames are full - any page-fault casues page-replacement!
-	for(;cur < STRING_LEN;cur++){
-		page_no = ref_str[cur];
-		frame_no = locate(frames, size, page_no);
-		if(frame_no >= 0)//already exists, no page-fault, no need for replacement!
-			continue;
-		//look at ref_str[cur+1:cur+limit] to see which one has not been referred to the longest in the future
-		page_faults++;
-		//This is the very first line that is differnet in other replacement algorithms
-		unsigned unused = (1<<size) - 1;//binary 1111111: all bits are one!
-		int victim;//frame index that must be swapped out of memory!
-		for(int k = 1; k <= limit && unused && cur + k < STRING_LEN;k++){
-			victim = locate(frames, size, ref_str[cur + k]);
-			if(victim >= 0)// in the memory
-				unused &= ~(1<<victim);//set the unused bit for victim to zero
-		}
-		if(!unused)//ALL frames will be refered to in the future... 
-			frames[victim] = page_no;//victim is the one that is referred the latest!
-		else{//At-least one frame won't be referred to in the future!
-			victim = 0;
-			while(unused % 2 == 0)//find the non-zero bit to find out the useless frame!
-				unused = unused >> 1, victim++;
-			frames[victim] = page_no;//useless frame is the victim!
-		}    
-	} 
-	return page_faults;
+
+// FIFO page replacement algorithm
+int fifo(int* ref_str, int size) {
+    int page_faults = 0;
+    int frames[size];
+    int front = 0;
+
+    for (int i = 0; i < size; i++)
+        frames[i] = -1;
+
+    for (int i = 0; i < STRING_LEN; i++) {
+        int page_no = ref_str[i];
+        int frame_no = locate(frames, size, page_no);
+        if (frame_no >= 0)
+            continue;
+
+        frames[front] = page_no;
+        front = (front + 1) % size;
+        page_faults++;
+    }
+    return page_faults;
 }
+
+// LRU page replacement algorithm
+int lru(int* ref_str, int size) {
+    int page_faults = 0;
+    int frames[size];
+    int last_used[size];
+
+    for (int i = 0; i < size; i++) {
+        frames[i] = -1;
+        last_used[i] = -1;
+    }
+
+    for (int i = 0; i < STRING_LEN; i++) {
+        int page_no = ref_str[i];
+        int frame_no = locate(frames, size, page_no);
+
+        if (frame_no >= 0) {
+            last_used[frame_no] = i;
+            continue;
+        }
+
+        page_faults++;
+        int lru_index = 0;
+        for (int j = 1; j < size; j++) {
+            if (last_used[j] < last_used[lru_index])
+                lru_index = j;
+        }
+
+        frames[lru_index] = page_no;
+        last_used[lru_index] = i;
+    }
+    return page_faults;
+}
+
+// Second Chance page replacement algorithm
+int second_chance(int* ref_str, int size) {
+    int page_faults = 0;
+    int frames[size];
+    int ref_bits[size];
+    int pointer = 0;
+
+    for (int i = 0; i < size; i++) {
+        frames[i] = -1;
+        ref_bits[i] = 0;
+    }
+
+    for (int i = 0; i < STRING_LEN; i++) {
+        int page_no = ref_str[i];
+        int frame_no = locate(frames, size, page_no);
+
+        if (frame_no >= 0) {
+            ref_bits[frame_no] = 1;
+            continue;
+        }
+
+        page_faults++;
+        while (ref_bits[pointer] == 1) {
+            ref_bits[pointer] = 0;
+            pointer = (pointer + 1) % size;
+        }
+
+        frames[pointer] = page_no;
+        ref_bits[pointer] = 1;
+        pointer = (pointer + 1) % size;
+    }
+    return page_faults;
+}
+
+// Main function
 int main(int argc, char* argv[]) {
-	srand(time(NULL));
-	if(handle_clas(argc, argv)){
-		return 1;
-		fprintf(stderr, "Fatal error: CLAs aren't passed correctly.\n");
-	}
-	else
-		fprintf(stderr, "CLAs are P = %d, locus size = %d, m = %d, transition prob. = %f\n", P, e, m, t);
-	int*ref_str = (int*) malloc(STRING_LEN*sizeof(int));
-	ref_string_generator(ref_str);
-	int optimal_page_fault = optimal(ref_str, ALLOCATED_FRAMES, e * m);
-	printf("Optimal page replacement causes %d page faults\n", optimal_page_fault);
-	//do the same for other algorithms!
-	return 0;
+    if (argc != 9) {
+        fprintf(stderr, "Usage: %s -P <P> -e <e> -m <m> -t <t>\n", argv[0]);
+        return 1;
+    }
+
+    int P = 0, e = 0, m = 0;
+    double t = 0.0;
+
+    for (int i = 1; i < argc; i += 2) {
+        if (strcmp(argv[i], "-P") == 0)
+            P = atoi(argv[i + 1]);
+        else if (strcmp(argv[i], "-e") == 0)
+            e = atoi(argv[i + 1]);
+        else if (strcmp(argv[i], "-m") == 0)
+            m = atoi(argv[i + 1]);
+        else if (strcmp(argv[i], "-t") == 0)
+            t = atof(argv[i + 1]);
+        else {
+            fprintf(stderr, "Invalid argument: %s\n", argv[i]);
+            return 1;
+        }
+    }
+
+    int* ref_str = (int*)malloc(STRING_LEN * sizeof(int));
+    ref_string_generator(ref_str, P, e, m, t);
+
+    int optimal_faults = optimal(ref_str, ALLOCATED_FRAMES, e * m);
+    int fifo_faults = fifo(ref_str, ALLOCATED_FRAMES);
+    int lru_faults = lru(ref_str, ALLOCATED_FRAMES);
+    int second_chance_faults = second_chance(ref_str, ALLOCATED_FRAMES);
+
+    printf("Page faults:\n");
+    printf("Optimal: %d\n", optimal_faults);
+    printf("FIFO: %d\n", fifo_faults);
+    printf("LRU: %d\n", lru_faults);
+    printf("Second Chance: %d\n", second_chance_faults);
+
+    free(ref_str);
+    return 0;
 }
